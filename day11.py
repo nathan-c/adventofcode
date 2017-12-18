9a2091a04a5b4f4e6ba9e9bfc70df0d5b089bbd5
Nathan Collard <n.w.j.collard@gmail.com>
2017-12-18 22:03:19 +0000

    Tweak day 11

--- day11.py
+++ day11.py
@@ -9,17 +9,17 @@ def run(path):
     max_manhattan = 0
     x, y, z = 0, 0, 0
     direction_map = {
-        'ne': lambda x, y, z: (x + 1, y + 1, z),
-        'sw': lambda x, y, z: (x - 1, y - 1, z),
-        'n': lambda x, y, z: (x, y + 1, z + 1),
-        's': lambda x, y, z: (x, y - 1, z - 1),
-        'nw': lambda x, y, z: (x, y - 1, z + 1),
-        'se': lambda x, y, z: (x, y + 1, z - 1),
+        'ne': lambda x, y, z: (x + 0.5, y + 0.5, z),
+        'sw': lambda x, y, z: (x - 0.5, y - 0.5, z),
+        'n': lambda x, y, z: (x, y + 0.5, z + 0.5),
+        's': lambda x, y, z: (x, y - 0.5, z - 0.5),
+        'nw': lambda x, y, z: (x, y - 0.5, z + 0.5),
+        'se': lambda x, y, z: (x, y + 0.5, z - 0.5),
     }
     for direction in path:
         x, y, z = direction_map[direction](x, y, z)
-        max_manhattan = max(manhattan_distance(x, y, z) // 2, max_manhattan)
-    return manhattan_distance(x, y, z) // 2, max_manhattan
+        max_manhattan = max(manhattan_distance(x, y, z), max_manhattan)
+    return manhattan_distance(x, y, z), max_manhattan
 
 
 def main():
--- test_day11.py
+++ test_day11.py
@@ -2,7 +2,7 @@ import day11
 
 
 def test_run():
-    assert day11.run(['ne', 'ne', 'ne']) == 3
-    assert day11.run(['ne', 'ne', 'sw', 'sw']) == 0
-    assert day11.run(['ne', 'ne', 's', 's']) == 2
-    assert day11.run(['se', 'sw', 'se', 'sw', 'sw']) == 3
+    assert day11.run(['ne', 'ne', 'ne'])[0] == 3
+    assert day11.run(['ne', 'ne', 'sw', 'sw'])[0] == 0
+    assert day11.run(['ne', 'ne', 's', 's'])[0] == 2
+    assert day11.run(['se', 'sw', 'se', 'sw', 'sw'])[0] == 3
