3ce2d2e76fdbca8b8ed8b6c5ac8a3321cc4668a3
Nathan Collard <n.w.j.collard@gmail.com>
2017-12-27 20:17:43 +0000

    begin day 14

--- day14.py
+++ day14.py
@@ -17,10 +17,10 @@ def run_part1(key):
     return sum_board
 
 
-def run_part1(key):
+def run_part2(key):
     ''' This is really slow :( '''
     num_regions = 0
-    #previous_row = ''.join(0 for x in range(128))
+    # previous_row = ''.join(0 for x in range(128))
     board = []
     for i in range(128):
         key_i = '{}-{}'.format(key, i)
@@ -29,39 +29,45 @@ def run_part1(key):
         # get hex output
         hex_data = day10.format_output(data, '02x')
         # convert to binary then sum all digits
-        row = ''.join(b for b in format(int(x, 16), '04b')) for x in hex_data)
-        board.append(row)
-    for row in board:
-        for col in row:
-            if board[row][col] == '1':
-                fill_region(board, row, col)
-                num_regions += 1
-    return sum_board
+        row = ''.join(format(int(x, 16), '04b') for x in hex_data)
+        board.append(list(row))
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            num_regions += fill_region(board, row, col)
+    return num_regions
 
 
 def fill_region(board, row, col):
+    if board[row][col] != '1': return 0
+    q=[(row, col)]
+    while q:
+        r, c=q.pop()
+        for child in get_joining(board, r, c):
+            q.append(child)
+        board[r][c]='0'
+    return 1
 
 
 
 def get_joining(board, row, col):
-    tl = row - 1, col - 1
-    cl = row, col - 1
-    bl = row + 1, col - 1
-    bc = row + 1, col
-    br = row + 1, col + 1
-    cr = row, col + 1
-    tr = row - 1, col + 1
-    tc = row - 1, col
-    possible_vals = [tl, cl, bl, bc, br, cr, tr, tc]
-    height = len(board)
-    width = len(board[0])
+    tl=row - 1, col - 1
+    cl=row, col - 1
+    bl=row + 1, col - 1
+    bc=row + 1, col
+    br=row + 1, col + 1
+    cr=row, col + 1
+    tr=row - 1, col + 1
+    tc=row - 1, col
+    possible_vals=[tl, cl, bl, bc, br, cr, tr, tc]
+    height=len(board)
+    width=len(board[0])
     for r, c in possible_vals:
-        if r > 0 and r < height and c> 0 and r < width:
+        if r >= 0 and r < height and c >= 0 and r < width:
             yield r, c
 
 
 def main():
-    print(run('hxtvlmkl'))
+    print(run_part1('hxtvlmkl'))
 
 
 if __name__ == '__main__':
--- test_day14.py
+++ test_day14.py
@@ -1,7 +1,10 @@
 import day14
 
-def test_run():
-    assert day14.run("flqrgnkx") == 8108
+def test_run_part1():
+    assert day14.run_part1("flqrgnkx") == 8108
+
+def test_run_part2():
+    assert day14.run_part2("flqrgnkx") == 1242
 
 def test_formatting():
     assert format(1,'04b') == '0001'
