cbf37f2d75f02ddfa40c8ce797d18355740bb2ad
Nathan Collard <n.w.j.collard@gmail.com>
2018-01-21 14:00:15 +0000

    Complete day 24

--- day24.py
+++ day24.py
@@ -8,6 +8,7 @@ class GraphNode(object):
         self.part = part
         self.weight = part[0] + part[1]
         self.next_port = next_port
+        self.path = set([part])
 
     def add_child(self, child):
         if child.parent:
@@ -15,15 +16,15 @@ class GraphNode(object):
         self.children.append(child)
         child.parent = self
         child.weight += self.weight
+        child.path |= self.path
 
     def can_connect(self, part):
-        return True if part[0] == self.part[self.next_port] or part[1] == self.part[self.next_port] else False
+        return True if part not in self.path and (part[0] == self.part[self.next_port] or part[1] == self.part[self.next_port]) else False
 
 
 def build_graph(parts):
     root = GraphNode((0, 0), 0)
-    partial_parts = {'': parts}
-    _build_graph(root, [], partial_parts)
+    _build_graph(parts, root)
     return root
 
 
@@ -36,20 +37,27 @@ def get_strongest_bridge(node):
     return strongest
 
 
-def _build_graph(graph, path, partial_parts):
-    parts = partial_parts[','.join(path)]
+def get_longest_bridge(node, current_height=0):
+    weight = node.weight
+    height = current_height
+    for child in node.children:
+        new_weight, new_height = get_longest_bridge(child, current_height + 1)
+        if new_height == height and new_weight > weight:
+            weight = new_weight
+        if new_height > height:
+            weight = new_weight
+            height = new_height
+
+    return weight, height
+
+
+def _build_graph(parts, graph):
     for i, part in enumerate(parts):
         if graph.can_connect(part):
             next_port = 1 if part[0] == graph.part[graph.next_port] else 0
             new_node = GraphNode(part, next_port)
             graph.add_child(new_node)
-            child_path = sorted(path + [str(part[2])])
-            child_path_string = ','.join(child_path)
-            if child_path_string not in partial_parts:
-                new_parts = parts[:i] + parts[i + 1:]
-                partial_parts[child_path_string] = new_parts
-
-            _build_graph(new_node, child_path, partial_parts)
+            _build_graph(parts, new_node)
 
 
 def run_part1(parts):
@@ -57,14 +65,20 @@ def run_part1(parts):
     return get_strongest_bridge(root)
 
 
+def run_part2(parts):
+    root = build_graph(parts)
+    return get_longest_bridge(root)[0]
+
+
 def parse_input(lines):
-    return [[int(c) for c in line.strip().split('/')] + [i] for i, line in enumerate(lines) if line.strip()]
+    return [tuple([int(c) for c in line.strip().split('/')] + [i]) for i, line in enumerate(lines) if line.strip()]
 
 
 def main():
     with load_file.open_file('day24.input.txt') as f:
         parts = parse_input(f)
-        print(run_part1(parts))
+        #print(run_part1(parts))
+        print(run_part2(parts))
 
 
 if __name__ == '__main__':
--- test_day24.py
+++ test_day24.py
@@ -28,3 +28,18 @@ def test_get_strongest_bridge():
     ]
     root = day24.build_graph(parts)
     assert day24.get_strongest_bridge(root) == 31
+
+
+def test_get_longest_bridge():
+    parts = [
+        (0, 2, 0),
+        (2, 2, 1),
+        (2, 3, 2),
+        (3, 4, 3),
+        (3, 5, 4),
+        (0, 1, 5),
+        (10, 1, 6),
+        (9, 10, 7)
+    ]
+    root = day24.build_graph(parts)
+    assert day24.get_longest_bridge(root)[0] == 19
