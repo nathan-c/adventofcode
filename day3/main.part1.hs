9c73c23fcda6f79f9fadfbba2071738ec9396d2f
nathan-c <nathan-c@hotmail.co.uk>
2018-12-05 07:29:53 +0000

    fixed day3 part 1

--- day3/main.part1.hs
+++ day3/main.part1.hs
@@ -4,32 +4,38 @@ import           Data.Maybe
 
 main = do
     content <- readFile "./input.txt"
-    let points = concat [ getPoints . parseLineShort $ l | l <- lines content ]
-    let pointCount = incrementCountsFromPoints M.empty points
-    let overlappingPoints = M.filter (>1) pointCount
+    let pointsPerLine = [ getPoints . parseLineShort $ l | l <- lines content ]
+    --print pointsPerLine
+    let points            = concat pointsPerLine
+    let pointCount        = incrementCountsFromPoints M.empty points
+    --print pointCount
+    let overlappingPoints = M.filter (> 1) pointCount
+    --print overlappingPoints
     print $ M.size overlappingPoints
 
-incrementCountsFromPoints :: M.Map (Int, Int) Int -> [(Int, Int)] -> M.Map (Int, Int) Int
+incrementCountsFromPoints
+    :: M.Map (Int, Int) Int -> [(Int, Int)] -> M.Map (Int, Int) Int
 incrementCountsFromPoints m [] = m
 incrementCountsFromPoints m (p : ps) =
     incrementCountsFromPoints (M.insertWith (+) p 1 m) ps
 
 getPoints :: (Int, Int, Int, Int) -> [(Int, Int)]
-getPoints (x, y, w, h) = [ (x, y) | x <- [x .. (x + w)], y <- [y .. (y + h)] ]
+getPoints (x, y, w, h) =
+    [ (x, y) | x <- [x .. (x + w - 1)], y <- [y .. (y + h - 1)] ]
 
 slice from to xs = read (take (to - from + 1) (drop from xs)) :: Int
 
 parseLine :: String -> (Int, Int, Int, Int, Int)
 parseLine l =
     let idStart = 1
-        idEnd   = (fromMaybe 0 $ '@' `elemIndex` l) - 2
-        xStart  = (fromMaybe 0 $ '@' `elemIndex` l) + 2
-        xEnd    = (fromMaybe 0 $ ',' `elemIndex` l) - 1
-        yStart  = (fromMaybe 0 $ ',' `elemIndex` l) + 1
-        yEnd    = (fromMaybe 0 $ ':' `elemIndex` l) - 1
-        wStart  = (fromMaybe 0 $ ':' `elemIndex` l) + 2
-        wEnd    = (fromMaybe 0 $ 'x' `elemIndex` l) - 1
-        hStart  = (fromMaybe 0 $ 'x' `elemIndex` l) + 1
+        idEnd   = getIndex '@' - 2
+        xStart  = getIndex '@' + 2
+        xEnd    = getIndex ',' - 1
+        yStart  = getIndex ',' + 1
+        yEnd    = getIndex ':' - 1
+        wStart  = getIndex ':' + 2
+        wEnd    = getIndex 'x' - 1
+        hStart  = getIndex 'x' + 1
         hEnd    = (length l) - 1
     in  ( slice idStart idEnd l
         , slice xStart  xEnd  l
@@ -37,21 +43,23 @@ parseLine l =
         , slice wStart  wEnd  l
         , slice hStart  hEnd  l
         )
+    where getIndex x = fromMaybe 0 $ x `elemIndex` l
 
 
 parseLineShort :: String -> (Int, Int, Int, Int)
 parseLineShort l =
-    let xStart  = (fromMaybe 0 $ '@' `elemIndex` l) + 2
-        xEnd    = (fromMaybe 0 $ ',' `elemIndex` l) - 1
-        yStart  = (fromMaybe 0 $ ',' `elemIndex` l) + 1
-        yEnd    = (fromMaybe 0 $ ':' `elemIndex` l) - 1
-        wStart  = (fromMaybe 0 $ ':' `elemIndex` l) + 2
-        wEnd    = (fromMaybe 0 $ 'x' `elemIndex` l) - 1
-        hStart  = (fromMaybe 0 $ 'x' `elemIndex` l) + 1
-        hEnd    = (length l) - 1
+    let xStart = getIndex '@' + 2
+        xEnd   = getIndex ',' - 1
+        yStart = getIndex ',' + 1
+        yEnd   = getIndex ':' - 1
+        wStart = getIndex ':' + 2
+        wEnd   = getIndex 'x' - 1
+        hStart = getIndex 'x' + 1
+        hEnd   = (length l) - 1
     in  ( slice xStart xEnd l
         , slice yStart yEnd l
         , slice wStart wEnd l
         , slice hStart hEnd l
         )
+    where getIndex x = fromMaybe 0 $ x `elemIndex` l
 
