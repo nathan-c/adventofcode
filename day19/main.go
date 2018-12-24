170e2a848e24a18e707f3091e8c1ab920db9f109
nathan-c <nathan-c@hotmail.co.uk>
2018-12-24 23:24:39 +0000

    struggling with day19

--- day19/main.go
+++ day19/main.go
@@ -10,43 +10,44 @@ import (
 )
 
 func main() {
-	//run(make([]int, 6, 6))
+	run(make([]int, 6, 6))
 	part2 := make([]int, 6, 6)
 	part2[0] = 1
-	run(part2)
+	//run(part2)
 }
 
 func run(register []int) {
 	instructions := buildInstructionMap()
-	program, ip := parseInput(instructions)
+	program, ipRegister := parseInput(instructions)
 	programHitCount := make([]int, len(program), len(program))
 	hotLoopPrint := 0
+	ip := 0
 	// reg4 := 0
 	for {
-		i := register[ip]
+		register[ipRegister] = ip
 
-		// if i == 9 && register[2] < register[4] {
+		// if ip == 9 && register[2] < register[4] {
 		// 	//shortcut!
-		// 	//fmt.Printf("%v %v %v\n", hotLoopPrint, register, programHitCount)
-		// 	register[2] = register[4]
-		// 	register[ip]++
+		// 	register[2] = register[4] + 1
+		// 	register[1] = 1
+		// } else if ip == 13 && register[5] < register[4] {
+		// 	register[1] = 1
+		// 	register[5] = register[4] + 1
 		// } else
-		if i < len(program) && i >= 0 {
-			programHitCount[i]++
-			inst := program[i]
-			if inst.c == ip {
-				fmt.Printf("%v %v %v\n", hotLoopPrint, register, programHitCount)
-			}
+		if ip < len(program) && ip >= 0 {
+			programHitCount[ip]++
+			inst := program[ip]
 			inst.i(register, inst.a, inst.b, inst.c)
-			register[ip]++
 		} else {
 			break
 		}
+		ip = register[ipRegister]
+		ip++
 		// if register[4] != reg4 {
 		// 	fmt.Printf("%v %v %v\n", hotLoopPrint, register, programHitCount)
 		// 	reg4 = register[4]
 		// }
-		//fmt.Printf("%v %v %v\n", hotLoopPrint, register, programHitCount)
+		fmt.Printf("%v %v %v\n", hotLoopPrint, register, programHitCount)
 		hotLoopPrint++
 		// if hotLoopPrint < 253234000 && hotLoopPrint > 253233000 {
 		// 	fmt.Printf("%v %v\n", register, programHitCount)
